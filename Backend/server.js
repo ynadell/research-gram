a2bf2bab4f6e51ff4719d9dfb52433f831889a20
Yashwanth Nadella <61127833+ynadell@users.noreply.github.com>
2025-03-13 20:39:12 -0400

    made the default backend port as 5001

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,6 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 // API Routes
 app.use("/api", apiRoutes);
 
@@ -16,5 +15,5 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
